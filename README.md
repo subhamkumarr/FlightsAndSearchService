60e4b9caa9b9bffb9bc63db2a6ce532ff118b21b
subhamkumarr <kumarsubham75861@gmail.com>
2023-06-14 14:32:12 +0530

    Added airport models

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
      - Flight Table
      - Airport Table
      - City Table 
+
+     ## Tables
+
+     ### City -> id, name, created_at, updated_at
+     ### Airport -> id, name, address, city_id, created_at, updated_at
+        Relationship -> City has many airports and Airport belongs to a city (one to many relation
+        )
--- src/models/airport.js
+++ src/models/airport.js
@@ -2,9 +2,8 @@
 const {
   Model
 } = require('sequelize');
-const airport = require('./airport');
 module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
+  class Airport extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -12,20 +11,25 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.Airport, {
-        foreignKey: 'cityId'
+      this.belongsTo(models.City, {
+        foreignKey: 'cityId',
+        onDelete: 'CASCADE'
       });
     }
   }
-  City.init({
+  Airport.init({
     name: {
-      type: DataTypes.STRING, 
-      allowNull: false,
-      unique: true
-    },
+      type: DataTypes.STRING,
+    allowNull: false
+  },
+    address: DataTypes.STRING,
+    cityId:{
+      type: DataTypes.INTEGER,
+    allowNull: false
+  }
   }, {
     sequelize,
-    modelName: 'City',
+    modelName: 'Airport',
   });
-  return City;
+  return Airport;
 };
